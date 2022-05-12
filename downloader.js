76f9723d80b283a8e88b83799c944cd194540788
Tyler Z <45436993+tzechco@users.noreply.github.com>
2022-05-12 14:25:12 -0400

    Update downloader.js

--- downloader.js
+++ downloader.js
@@ -1,1 +1,5 @@
-
+/** @param {NS} ns */
+export async function main(ns) {
+	await ns.wget('https://raw.githubusercontent.com/tzechco/bitburner-scripts/main/downloader.js', 'downloader.js');
+  ns.tprint('worked');
+}

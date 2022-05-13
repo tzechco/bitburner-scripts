6e315ca63473ef262bd4af97805d8ac734f37a6f
Tyler Z <45436993+tzechco@users.noreply.github.com>
2022-05-12 20:54:35 -0400

    Update updater.js

--- updater.js
+++ updater.js
@@ -1,14 +1,25 @@
 /** @param {NS} ns */
 export async function main(ns) {
-    if (ns.args[0]) {
-        await ns.wget(`https://raw.githubusercontent.com/tzechco/bitburner-scripts/main/${ns.args[0]}`, `/temp/${ns.args[0]}`)
+    if(args[0] == 'ow') {
+        await ns.mv('home', 'updater.js', '/temp/updater.js');
+        await ns.rm('/temp/updater.js');
+        ns.tprint('Updates Complete!');
+
+    } else if (ns.args[0]) {
+        await ns.wget(`https://raw.githubusercontent.com/tzechco/bitburner-scripts/main/${ns.args[0]}`, `/temp/${ns.args[0]}`);
         if (ns.read(ns.args[0]) !== ns.read(`/temp/${ns.args[0]}`)) {
-            await ns.mv('home', ns.args[0], `/temp/${ns.args[0]}`)
+            await ns.mv('home', ns.args[0], `/temp/${ns.args[0]}`);
             return ns.exec(ns.args[0], 'home', 1, 'skip');
         }
     }
-    let files = ['autohack.js', 'updater.js', 'runners/hack.js', 'runners/grow.js', 'runners/weaken.js']
-    for (let index = 0; index < files.length; ++index) {
-        await ns.wget(`https://raw.githubusercontent.com/tzechco/bitburner-scripts/main/${files[index]}`, files[index])
+    let files = ['/autohack.js', '/updater.js', '/runners/hack.js', '/runners/grow.js', '/runners/weaken.js'];
+    for (let i = 0; i < files.length; ++i) {
+        if(files[i] == '/updater.js') {
+            ns.tprint(`Downloading ${files[i]}`)
+            await ns.wget(`https://raw.githubusercontent.com/tzechco/bitburner-scripts/main${files[i]}`, `/temp${files[i]}`);
+        }
+        ns.tprint(`Downloading ${files[i]}`)
+        await ns.wget(`https://raw.githubusercontent.com/tzechco/bitburner-scripts/main${files[i]}`, files[i]);
     }
+    ns.exec('/temp/updater.js', 'home', 1, 'ow');
 }

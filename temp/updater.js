49933fae8fecb4a1236d58f03cd296b4d6d4a461
Tyler Z <45436993+tzechco@users.noreply.github.com>
2022-05-12 21:58:58 -0400

    Delete updater.js

--- temp/updater.js
+++
@@ -1,9 +0,0 @@
-/** @param {NS} ns */
-export async function main(ns) {
-    let files = ['autohack.js', '/runners/hack.js', '/runners/grow.js', '/runners/weaken.js'];
-    for (let i = 0; i < files.length; ++i) {
-        ns.tprintf(`Downloading ${files[i]}`);
-        await ns.rm(files[i]);
-        await ns.wget(`https://raw.githubusercontent.com/tzechco/bitburner-scripts/main/${files[i]}`, files[i]);
-    }
-}

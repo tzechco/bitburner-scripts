a3e04260f4a7862fb9ede554a614384be98f60e6
Tyler Z <45436993+tzechco@users.noreply.github.com>
2022-05-13 14:26:03 -0400

    Update updater.js

--- updater.js
+++ updater.js
@@ -4,7 +4,7 @@ export async function main(ns) {
         await ns.wget(`https://raw.githubusercontent.com/tzechco/bitburner-scripts/main/${ns.args[0]}`, ns.args[0]);
         return ns.exec(ns.args[0], 'home', 1, 'skip');
     }
-    let files = ['autohack.js', 'updater.js', '/runners/hack.js', '/runners/grow.js', '/runners/weaken.js'];
+    let files = ['autohack.js', 'updater.js', 'rootall.js', '/runners/hack.js', '/runners/grow.js', '/runners/weaken.js'];
     for (let i = 0; i < files.length; ++i) {
         ns.tprint(`Downloading ${files[i]}`);
         await ns.wget(`https://raw.githubusercontent.com/tzechco/bitburner-scripts/main/${files[i]}`, files[i]);

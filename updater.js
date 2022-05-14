01eff696b95d8ce652435c5c7141aa8054742e3d
tzechco <45436993+tzechco@users.noreply.github.com>
2022-05-14 10:49:41 -0400

    Update updater.js

--- updater.js
+++ updater.js
@@ -1,17 +1,25 @@
 /** @param {NS} ns */
 export async function main(ns) {
-    if (ns.args[0]) {
+    if (ns.args[0] == 'update') {
+        let files = ['autohack.js', 'rootall.js', '/runners/hack.js', '/runners/grow.js', '/runners/weaken.js'];
+        for (let i = 0; i < files.length; ++i) {
+            ns.tprint(`Downloading ${files[i]}`);
+            await ns.wget(`https://raw.githubusercontent.com/tzechco/bitburner-scripts/main/${files[i]}`, files[i]);
+        }
+        if (!ns.fileExists('launchOptions.txt')) {
+            let autoUpdate = await ns.prompt('Would you like enable auto updates?');
+            await ns.write('launchOptions.txt', `{"autoUpdate":${autoUpdate}}`, 'w');
+        }
+        ns.tprint('Update Complete!');
+    }
+    else if (ns.args[0]) {
         await ns.wget(`https://raw.githubusercontent.com/tzechco/bitburner-scripts/main/${ns.args[0]}`, ns.args[0]);
         return ns.exec(ns.args[0], 'home', 1, 'skip');
     }
-    let files = ['autohack.js', 'updater.js', 'rootall.js', '/runners/hack.js', '/runners/grow.js', '/runners/weaken.js'];
-    for (let i = 0; i < files.length; ++i) {
-        ns.tprint(`Downloading ${files[i]}`);
-        await ns.wget(`https://raw.githubusercontent.com/tzechco/bitburner-scripts/main/${files[i]}`, files[i]);
+    ns.tprint('Downloading updater.js')
+    await ns.wget(`https://raw.githubusercontent.com/tzechco/bitburner-scripts/main/updater.js`, 'updater.js')
+    if (Math.floor((ns.getServerMaxRam('home') - ns.getServerUsedRam('home')) / ns.getScriptRam('updater.js')) > 0) {
+        return ns.exec('updater.js', 'home', 1, 'update');
     }
-    if (!ns.fileExists('launchOptions.txt')) {
-        let autoUpdate = await ns.prompt('Would you like enable auto updates?');
-        await ns.write('launchOptions.txt', `{"autoUpdate":${autoUpdate}}`, 'w');
-    }
-    ns.tprint('Update Complete!');
+    ns.tprint('Not enough ram to complete update.')
 }
